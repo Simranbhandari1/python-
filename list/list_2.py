55c79c133a5977087a7c2c563d93a70cad25afe2
Simranbhandari1 <simranbhandari604@gmail.com>
2025-09-08 22:44:25 +0530

    Python conecpts

---
+++ list/list_2.py
@@ -0,0 +1,10 @@
+original = [[1, 2], [3, 4]]#these are python nested list
+print(original[0])      # [1, 2]   (first inner list)
+print(original[1])      # [3, 4]   (second inner list)
+
+print(original[0][0])   # 1  (first element of first inner list)
+print(original[0][1])   # 2  (second element of first inner list)
+
+print(original[1][0])   # 3  (first element of second inner list)
+print(original[1][1])   # 4  (second element of second inner list)
+
