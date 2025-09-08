b9ceef288b5fc1085f5302837063c2a337706f0e
Simranbhandari1 <simranbhandari604@gmail.com>
2025-09-08 22:40:24 +0530

    Python conecpts

---
+++ list/list.py
@@ -0,0 +1,8 @@
+#list mutable
+friend = ["Apple" ,"banana" ,"4","rohan","hey"]
+print(friend)
+print(friend[4])
+print(friend[1:3])#last one is excluded it is slicing
+print(friend[1:])
+friend[3]="you canassign items like this as list are mutable while strings are immutable "
+print(friend)
