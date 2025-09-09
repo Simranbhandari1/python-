5e96d9e455ed65bf77e8b6a02263673e81c3440b
Simranbhandari1 <simranbhandari604@gmail.com>
2025-09-09 21:32:28 +0530

    Python conecpts

---
+++ list/list_10.py
@@ -0,0 +1,15 @@
+marks = {
+    "harry" : 100,
+    "Subham" : 200,
+    "Rohan" : 23,
+    "li":[1,2,3,4],
+    0:"hey"    
+}
+print(marks, type(marks))
+print(marks["harry"])
+# print(marks["Subham"])
+# print(marks["Rohan"])
+print(marks["li"])
+print(marks[0])
+
+# it is unordered,mutable(change the origional tuple),indexed,cant conain duplicate key
